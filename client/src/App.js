35425404b396b8316f0c11b4ad46137f422b7acb
Aydin Kuru <aydinkuru81@gmail.com>
2022-07-25 23:36:31 +0200

    backend

--- client/src/App.js
+++ client/src/App.js
@@ -1,9 +1,12 @@
+//import logo from './logo.svg';
 import "./App.css";
 
 function App() {
   return (
     <div className="App">
-      <h1> Upload File</h1>
+      <header className="App-header">
+        <p>Upload a file</p>
+      </header>
     </div>
   );
 }
